f7f92bcd35717571f99e377fa6c3309a7eca3c85
Max <maxduma16@gmail.com>
2023-03-09 19:37:14 +0200

    refactor

--- src/App.vue
+++ src/App.vue
@@ -212,6 +212,18 @@ export default {
   },
 
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem('cryptonomicon-list');
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
@@ -276,7 +288,14 @@ export default {
   watch: {
     filter() {
       this.page = 1;
-    }
+      const {protocol, host, pathname} = window.location;
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`);
+    },
+    page() {
+      this.page = 1;
+      const {protocol, host, pathname} = window.location;
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`);
+    },
   }
 };
 </script>
