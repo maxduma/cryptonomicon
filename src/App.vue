4b215a3d7d99a321e406e676598e01b531d9b573
Max <maxduma16@gmail.com>
2023-03-09 18:06:37 +0200

    add feature filter and others

--- src/App.vue
+++ src/App.vue
@@ -92,10 +92,20 @@
       </section>
 
       <template v-if="tickers.length">
+          <hr class="w-full border-t border-gray-600 my-4" />
+        <div>
+          <button  class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+            next
+          </button>
+          <button class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+            back
+          </button>
+          <div>Filter:<input v-model="filter" /></div>
+        </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t of tickers"
+            v-for="t of filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -189,6 +199,8 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
+      page: 1,
+      filter: ''
     };
   },
 
@@ -203,6 +215,9 @@ export default {
   },
 
   methods: {
+    filteredTickers() {
+      return this.tickers.filter(ticker => ticker.name.includes(this.filter))
+    },
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
